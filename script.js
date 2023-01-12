811f0b245eab9e388796144686119b20a81eeec5
mxxglad <120576248+mxxglad@users.noreply.github.com>
2023-01-12 11:22:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
     $('.burger-container').click(function () {
         $('.header__menu').toggleClass('header__menu--active');
         $('.header__burger').toggleClass('header__burger--active');
-        $('.burger-container').toggleClass('burger-container--active--768');
+        $('.burger-container').toggleClass('burger-container--active');
     });
     $('.header__lang-nav').click(function () {
         $('.header__lang-list').toggleClass('header__lang-list--active');
     });
+    $(function () {
+        $(window).scroll(function () {
+            var top = $(document).scrollTop();
+            if (top > 60) $('.burger-container').addClass('burger-container--downed'), $('.burger__label').addClass('burger__label--downed');
+            else $('.burger-container').removeClass('burger-container--downed'), $('.burger__label').removeClass('burger__label--downed')});
+    });
 });
+
