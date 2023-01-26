b90cd1b4aa7714c35965ee3b1a6df4d4538c7a8f
mxxglad <120576248+mxxglad@users.noreply.github.com>
2023-01-26 14:04:52 +0200

    Add files via upload

---
+++ Ukrinform AI/script.js
@@ -0,0 +1,18 @@
+$('.header__lang-button').click(function(){
+    $('.lang-list').toggleClass('lang-list--active')
+});
+$('.more-button').click(function () {
+    $('.more__list').toggleClass('more__list--active')
+    $('.more__first-elem').toggleClass('more__first-elem--active')
+});
+$('.header__burger').click(function () {
+    $('.all-nav-aside').toggleClass('visible');
+    $('.more__list').toggleClass('more__list--active');
+});
+$(function () {
+    $(window).scroll(function () {
+        var top = $(document).scrollTop();
+        if (top > 42) $('.all-nav-aside').addClass('all-nav-aside--downed');
+        else $('.all-nav-aside').removeClass('all-nav-aside--downed')
+    });
+});
