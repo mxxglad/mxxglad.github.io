c3214c5157d39e765ab0e4b9f54f8371a4ea8d83
mxxglad <120576248+mxxglad@users.noreply.github.com>
2023-04-12 15:20:51 +0300

    Add files via upload

---
+++ D&M leasing/js/main.js
@@ -0,0 +1,51 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+    element.addEventListener('click', open);
+})
+
+function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function(item) {
+        item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function (item) {
+        item.classList.remove('tabs__content-item--active');
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
+
+
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
+
+
+
+
+$(document).ready(function () {
+    $('.header__burger').click(function () {
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu').toggleClass('menu--active');
+        $('.header__burger').toggleClass('header__burger--active');
+        $('.burger__line').toggleClass('burger__line--active');
+        $('body').toggleClass('body--locked');
+    });
+});
