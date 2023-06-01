bc13413af50fa6e2e5c529de97c78c9102aeb16f
mxxglad <120576248+mxxglad@users.noreply.github.com>
2023-06-01 09:14:04 +0300

    Add files via upload

--- Список покупок/script.js
+++ Список покупок/script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('input');
 const fillList = document.querySelector('#fill');
 const addButton = document.querySelector('#add');
-const resetButton = document.querySelector('#reset');
+const clearButton = document.querySelector('#clear');
 const rememberButton = document.querySelector('#remember')
 
 addButton.onclick = function () {
@@ -11,18 +11,18 @@ addButton.onclick = function () {
         fillList.appendChild(li);
 
 
-        let liArray = document.querySelectorAll('li');
+        let liArray = document.querySelectorAll('#fill li');
 
         input.value = '';
         document.querySelector('p').textContent = '';
-        resetButton.style.display = 'block';
+        clearButton.style.display = 'block';
         rememberButton.style.display = 'block';    
 
-        resetButton.onclick = function () {
+        clearButton.onclick = function () {
             liArray.forEach(function (li) {
                 li.remove();
             })
-            resetButton.style.display = 'none';
+            clearButton.style.display = 'none';
             rememberButton.style.display = 'none';
             document.querySelector('p').textContent = 'Список пуст';
         };
@@ -31,23 +31,22 @@ addButton.onclick = function () {
         rememberButton.onclick = function () {
             let groceries = [];
             for (let i = 0; i < liArray.length; i++){
-                groceries.push(liArray[i].textContent)
+                groceries.push(liArray[i].textContent);
+                window.localStorage.setItem('name', JSON.stringify(groceries));
             }
-            console.log(groceries)
-            window.localStorage.setItem('name', JSON.stringify(groceries));
-
         }
     }
 }
 
 
-const saveButton = document.querySelector('#saved')
+const savedButton = document.querySelector('#saved')
 const lastList = document.querySelector('#last')
+const resetButton = document.querySelector('#reset')
 
-saveButton.onclick = function () {
+savedButton.onclick = function () {
     let groceriesGot = JSON.parse(localStorage.getItem('name'));
-    console.log(groceriesGot);
-    for (let i = 0; i < groceriesGot.length; i++){
+    if (groceriesGot !== null) {
+        for (let i = 0; i < groceriesGot.length; i++){
         let li = document.createElement('li');
         li.textContent = groceriesGot[i];
         lastList.appendChild(li);
@@ -60,6 +59,17 @@ saveButton.onclick = function () {
                 this.style.textDecoration = 'line-through';
             }
         })
+    }
+    else document.querySelector('#h3').textContent = 'Последний список: Пусто'
+}
+
+resetButton.onclick = function () {
+    for (let i = 0; i < document.querySelectorAll('#last li').length; i++){
+        document.querySelectorAll('#last li')[i].remove();
+    }
+    window.localStorage.clear();
+    console.log(window.localStorage);
+    
 }
 
 
